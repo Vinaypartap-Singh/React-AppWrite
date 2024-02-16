a2357f62f4940cbf0b6e517a1aa65c8e22a5d27a
developervinay1 <developervsandhu@gmail.com>
2024-02-16 15:41:56 -0500

    Routing in application

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -1,3 +1,21 @@
-export default function ({ children, authenticated = true }) {
-  return <>{children}</>;
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+export default function ({ children, authentication = true }) {
+  const authStatus = useSelector((state) => state.auth.status);
+  const navigate = useNavigate();
+  const [loader, setLoader] = useState(true);
+
+  useEffect(() => {
+    // the logic is that authStatus not equal to authentication like not equal to true
+    if (authentication && authStatus !== authentication) {
+      navigate("/login");
+    } else if (!authentication && authStatus !== authentication) {
+      navigate("/");
+    }
+    setLoader(false);
+  }, [authStatus, authentication, navigate]);
+
+  return loader ? null : <>{children}</>;
 }
